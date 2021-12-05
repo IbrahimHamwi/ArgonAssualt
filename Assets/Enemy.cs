6d3e164b5f9591a5d7094482a41e99272f8a5b64
IbrahimHamwi <ibrahimhamwi_619@hotmail.com>
2021-12-05 03:18:24 +0300

    added score and hit points

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,11 +4,38 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVfx;
+    [SerializeField] Transform parent;
+    [SerializeField] int ScorePerHit = 15;
+    [SerializeField] int hitPoints = 2;
+    Scoreboard scoreboard;
+
+    void Start()
+    {
+        scoreboard = FindObjectOfType<Scoreboard>();
+    }
     void OnParticleCollision(GameObject other)
     {
-        Debug.Log($"{name}I'm hit! by {other.gameObject.name}");
+        ProcessHit();
+        if (hitPoints < 1)
+        {
+            KillEnemy();
+        }
+    }
 
+    private void KillEnemy()
+    {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
 
+    private void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVfx, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        hitPoints--;
+        scoreboard.IncreaseScore(ScorePerHit);
+    }
 }
